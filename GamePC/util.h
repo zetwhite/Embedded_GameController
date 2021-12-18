cf2e85b05bdcb94cf3687a3b19562995d574e27c
lcy8047 <lcy8047@gmail.com>
2021-12-18 20:42:54 +0900

    add clearMap drawBorder drawStr

--- GamePC/util.h
+++ GamePC/util.h
@@ -1,8 +1,11 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
-int init();
+int init        ( void );
 //void run();
-void close();
+void close      ( void );
+void drawBorder ( WINDOW *win );
+void clearMap   ( WINDOW *win );
+void drawStr    ( WINDOW *win, int y, int x, const char *s );
 
 #endif
