877c8ba590fcae6de0f9ea4f49ea8f7948d41775
lcy8047 <lcy8047@gmail.com>
2021-12-18 20:42:05 +0900

    add Snake Game

--- GamePC/main.cpp
+++ GamePC/main.cpp
@@ -2,13 +2,17 @@
 #include <string>
 #include "util.h"
 #include "init_page.h"
+#include "SnakeGame/snakeGame.h"
 
 int main(int argv, char** argc) {
     int init_status = init();
     int nxt_page_index = 0; 
 
     if(init_status == 0)
-        nxt_page_index = init_page();    
+        nxt_page_index = init_page();
+    if( nxt_page_index == 1 ) {
+        SnakeGame();
+    }
     close();
     printf("result is %d\n", nxt_page_index); 
     return 0;
