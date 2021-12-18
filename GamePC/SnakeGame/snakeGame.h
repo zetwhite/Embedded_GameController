6ea9817443c10aefb6756831faaba0655fa9eaaa
lcy8047 <lcy8047@gmail.com>
2021-12-18 20:46:27 +0900

    Snake Game

---
+++ GamePC/SnakeGame/snakeGame.h
@@ -0,0 +1,11 @@
+// public
+void SnakeGame          ( void );
+
+// private
+void snakeGameInit      ( void );
+void drawSnakeGameIntro ( void );
+void drawSnakeGameOver  ( void );
+void startSnakeGame     ( void );
+void endSnakeGame       ( void );
+void makeFood           ( void );
+void moveSnake          ( int dir );
