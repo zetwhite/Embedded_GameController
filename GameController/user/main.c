4dd7271f1416f326d29a29c51c1c5738b95f57c2
zetwhite <zetwhite@naver.com>
2021-12-18 21:59:52 +0900

    add basic bluetooth code and joystick, button interrupts

--- GameController/user/main.c
+++ GameController/user/main.c
@@ -1,7 +1,313 @@
-#include "lcd.h" 
+#include <stdio.h>
+#include "stm32f10x.h"
+#include "stm32f10x_exti.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_rcc.h"
+#include "misc.h"
+
+/* function prototype */
+void RCC_Configure(void);
+void GPIO_Configure(void);
+void USART1_2_Init(void);
+void NVIC_Configure(void);
+
+void Delay(void);
+
+void sendDataUART1(uint16_t data);
+void sendDataUART2(uint16_t data);
+
+
+void RCC_Configure(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); //for bluetooth
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); //for joystick 
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE); //for joystick, for buttons
+    
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE); //for button
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+}
+
+void GPIO_Configure(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    
+    //JoyStick Setting
+    GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_9); 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+    GPIO_Init(GPIOB, &GPIO_InitStructure); 
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+    GPIO_Init(GPIOE, &GPIO_InitStructure); 
+    
+    
+    //For Button 
+    GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_2 | GPIO_Pin_3); 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+    GPIO_Init(GPIOE, &GPIO_InitStructure); 
+    
+    /* UART1 pin setting (PC - board serial communication) */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; 
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    //RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+    /* UART2 pin setting (bluetooth communication)*/
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; 
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    //RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void USART1_Init(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+    
+    USART_Cmd(USART1, ENABLE);
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_Mode = (USART_CR1_RE | USART_CR1_TE | USART_CR1_UE);
+    USART_InitStructure.USART_WordLength = 0;
+    USART_InitStructure.USART_StopBits = 0;
+    USART_InitStructure.USART_Parity = 0;
+    USART_InitStructure.USART_HardwareFlowControl = 0;
+    USART_Init(USART1, &USART_InitStructure);
+    
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
+
+void USART2_Init(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    USART_Cmd(USART2, ENABLE);
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_Mode = (USART_CR1_RE | USART_CR1_TE | USART_CR1_UE);
+    USART_InitStructure.USART_WordLength = 0;
+    USART_InitStructure.USART_StopBits = 0;
+    USART_InitStructure.USART_Parity = 0;
+    USART_InitStructure.USART_HardwareFlowControl = 0;
+    USART_Init(USART2, &USART_InitStructure);
+        
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+}
+
+void EXTI_Configure(void){
+    EXTI_InitTypeDef EXTI_InitStructure; 
+    
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource0);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+ 
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource2);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line2;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource3);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line3;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource5);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line5;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+    
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource6);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line6;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+    
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource9);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line9;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+}
+
+void NVIC_Configure(void) {
+
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+    // UART1
+    NVIC_EnableIRQ(USART1_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    // UART2
+    NVIC_EnableIRQ(USART2_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+   
+    // Joystick Down
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+void EXTI0_IRQHandler(void) {
+
+	if (GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_0)) {
+                  USART_SendData(USART2, '0');
+                  printf("interrupted by PE0\n");
+	}
+        EXTI_ClearITPendingBit(EXTI_Line0);
+}
+
+void EXTI2_IRQHandler(void) {
+
+	if (GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_2)) {
+                  USART_SendData(USART2, '2');
+                  printf("interrupted by PE2\n");
+	}
+        EXTI_ClearITPendingBit(EXTI_Line2);
+}
+
+void EXTI3_IRQHandler(void) {
+
+	if (GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_3)) {
+                  USART_SendData(USART2, '3');
+                  printf("interrupted by PE3\n");
+	}
+        EXTI_ClearITPendingBit(EXTI_Line3);
+}
+
+void EXTI9_5_IRQHandler(void) {
+
+    if (EXTI_GetITStatus(EXTI_Line6) != RESET) {
+        if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_6)) {
+                  USART_SendData(USART2, '6');
+                  printf("interrupted by PB6\n"); 
+          }
+          EXTI_ClearITPendingBit(EXTI_Line6);
+    }
+    else if (EXTI_GetITStatus(EXTI_Line5) != RESET) {
+          if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_5)) {
+                  USART_SendData(USART2, '5');
+                  printf("interrupted by PB5\n"); 
+
+          }
+          EXTI_ClearITPendingBit(EXTI_Line5);
+    }
+    else if (EXTI_GetITStatus(EXTI_Line9) != RESET) {
+          if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9)) {
+                  USART_SendData(USART2, '9');
+                  printf("interrupted by PB9\n"); 
+          }
+          EXTI_ClearITPendingBit(EXTI_Line9);
+    }
+}
+
+void USART1_IRQHandler() {
+    uint16_t word;
+    if(USART_GetITStatus(USART1,USART_IT_RXNE)!= RESET){
+    	// the most recent received data by the USART1 peripheral
+        word = USART_ReceiveData(USART1);
+
+        sendDataUART2(word);
+        
+    	USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+    }
+} 
+
+void USART2_IRQHandler() {
+    uint16_t word;
+    if(USART_GetITStatus(USART2,USART_IT_RXNE)!= RESET){
+    	// the most recent received data by the USART2 peripheral
+        word = USART_ReceiveData(USART2);
+
+        // TODO implement
+        sendDataUART1(word);
+        // clear 'Read data register not empty' flag
+    	USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+    }
+}
+
+void Delay(void) {
+	int i;
+
+	for (i = 0; i < 2000000; i++) {}
+}
+
+void sendDataUART1(uint16_t data) {
+	/* Wait till TC is set */
+	USART_SendData(USART1, data);
+}
+
+void sendDataUART2(uint16_t data) {
+	/* Wait till TC is set */
+	USART_SendData(USART2, data);
+}
 
 int main(void)
 {
-  while(1) {} 
-  return 0; 
+
+    SystemInit();
+    RCC_Configure();
+    GPIO_Configure();
+    USART1_Init();
+    USART2_Init(); 
+    EXTI_Configure();   
+    NVIC_Configure();
+
+    while (1) {
+      
+    }
+    return 0;
 }
